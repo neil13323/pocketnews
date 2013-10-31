1e6c83ad069a5051da1268a740258b02cd9207ff
Administrator <administrator@ubuntu.ubuntu-domain>
2013-10-31 14:00:15 +0800

    初始化工程

---
+++ src/main/java/com/controllar/Controllar.java
@@ -0,0 +1,11 @@
+package com.controllar;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: administrator
+ * Date: 10/31/13
+ * Time: 1:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controllar {
+}
